d549fd05c7de62579f8226cf02e3eb8925bf127d
Dominik Hádl <doha@nodesagency.com>
2018-10-23 15:13:36 +0200

    Add interactors template

---
+++ VIPER Coordinators/Interactor.xctemplate/___FILEBASENAME___Models.swift
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+enum ___VARIABLE_sceneName___ {
+    enum Request { }
+    enum Response { }
+}
+
+extension ___VARIABLE_sceneName___.Request {
+
+}
+
+extension ___VARIABLE_sceneName___.Response {
+
+}
---
+++ VIPER Coordinators/Interactor.xctemplate/___FILEBASENAME___Protocols.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+// PRESENTER -> INTERACTOR
+protocol ___VARIABLE_sceneName___InteractorInput {
+    // func perform(_ request: ___VARIABLE_sceneName___.Request.Work)
+}
+
+// INTERACTOR -> PRESENTER (indirect)
+protocol ___VARIABLE_sceneName___InteractorOutput: class {
+    // func present(_ response: ___VARIABLE_sceneName___.Response.Work)
+}
